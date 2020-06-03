224547551bd7b525fd9f8c0d137c593a143d0008
Sam Tay <sam.chong.tay@gmail.com>
2020-06-02 23:34:11 -0700

    Parse CLI args with clap

--- TODO.md
+++ TODO.md
@@ -1,8 +1,5 @@
 # TODO
 
-1. Clap
-   - [setup](https://rust-lang-nursery.github.io/rust-cookbook/cli/arguments.html#parse-command-line-arguments)
-   - [handle version properly](https://docs.rs/clap/2.33.1/clap/macro.crate_version.html)
 2. Hit SO API
    - [json specs](https://rust-lang-nursery.github.io/rust-cookbook/encoding/complex.html#serialize-and-deserialize-unstructured-json)
    - [hitting api](https://rust-lang-nursery.github.io/rust-cookbook/web/clients/apis.html)
--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,37 @@
+use clap::clap_app;
+use clap::App;
+
+// TODO pull defaults from config file
+// may require dropping the macros
+fn mk_app<'a, 'b>() -> App<'a, 'b> {
+    clap_app!(so =>
+        (version: clap::crate_version!())
+        (author: clap::crate_authors!())
+        (about: clap::crate_description!())
+        (@arg site: -s --site +takes_value default_value("stackoverflow") "StackExchange site")
+        (@arg query: ... +required "Query to search")
+    )
+}
+
 fn main() {
-    println!("Hello, world!");
+    let matches = mk_app().get_matches();
+    println!("{:?}", matches);
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn test_cli() {
+        let m = mk_app().get_matches_from(vec![
+            "so", "--site", "meta", "how", "do", "I", "exit", "Vim",
+        ]);
+        println!("{:?}", m);
+        assert_eq!(m.value_of("site"), Some("meta"));
+        assert_eq!(
+            m.values_of("query").unwrap().collect::<Vec<_>>().join(" "),
+            "how do I exit Vim"
+        );
+    }
 }
