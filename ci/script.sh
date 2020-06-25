1c90502a3c4f735a7546a1147b100946f1a5af01
Sam Tay <sam.chong.tay@gmail.com>
2020-06-25 02:10:48 -0700

    Add CI courtesy of trust & cross

---
+++ ci/script.sh
@@ -0,0 +1,24 @@
+# This script takes care of testing your crate
+
+set -ex
+
+main() {
+    local features="--features reqwest/native-tls-vendored"
+    if [[ $TARGET =~ .*-freebsd ]]; then
+        features="--features reqwest/rustls-tls"
+    fi
+
+    cross build $features --target $TARGET
+    cross build $features --target $TARGET --release
+
+    if [ ! -z $DISABLE_TESTS ]; then
+        return
+    fi
+
+    cross test $features --target $TARGET
+}
+
+# we don't run the "test phase" when doing deploys
+if [ -z $TRAVIS_TAG ]; then
+    main
+fi
